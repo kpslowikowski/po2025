19eccbcb99bec0ee717d0ff3bba9baae7888b86d
Krzysztof <kslowikowski@student.agh.edu.pl>
2025-10-14 15:56:56 +0200

    Lab2.2

--- Lab2/src/Lotto.java
+++ Lab2/src/Lotto.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Lotto {
+    public static List<Integer> drawSix() {
+        List<Integer> pool = new ArrayList<>();
+    }
 }
